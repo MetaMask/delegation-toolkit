8ba4e7e81dd34aecec4aca62c0d0751428f804e1
MoMannn <tadej.vengust@consensys.net>
2025-08-05 15:34:51 +0200

    Add metamask permissions and better descriptions.

--- packages/permission-types/src/types.ts
+++ packages/permission-types/src/types.ts
@@ -1,4 +1,20 @@
+////////////////////////////////////////////////////
+// General Types
+////////////////////////////////////////////////////
+
+/**
+ * A hex-encoded string.
+ */
 export type Hex = `0x${string}`;
+  
+/**
+ * The types of keys that are supported for the following `key` and `keys` signer types.
+ */
+export type KeyType = "secp256r1" | "secp256k1" | "ed25519" | "schnorr";
+
+////////////////////////////////////////////////////
+// Signer Types
+////////////////////////////////////////////////////
 
 /**
  * A wallet is the signer for these permissions
@@ -8,11 +24,6 @@ export type WalletSigner = {
     type: "wallet";
     data: {};
 };
-  
-/**
- * The types of keys that are supported for the following `key` and `keys` signer types.
- */
-export type KeyType = "secp256r1" | "secp256k1" | "ed25519" | "schnorr";
   
 /**
  * A signer representing a single key.
@@ -52,56 +63,169 @@ export type AccountSigner = {
 
 export type Signer = WalletSigner | KeySigner | MultiKeySigner | AccountSigner;
 
+////////////////////////////////////////////////////
+// Permission Types
+////////////////////////////////////////////////////
+
 /**
  * A base permission type that all permissions must extend.
  * `isAdjustmentAllowed` defines a boolean value that allows DApp to define whether the "permission" can be attenuated–adjusted to meet the user's terms.
- * @param type - is an enum defined by the ERCs
- * @param isAdjustmentAllowed - is a boolean that indicates whether the permission can be adjusted.
- * @param data - is a record of the data that is associated with the permission, and the structure is defined by the ERCs.
+ * @property type - is an enum defined by the ERCs
+ * @property isAdjustmentAllowed - is a boolean that indicates whether the permission can be adjusted.
+ * @property data - is a record of the data that is associated with the permission, and the structure is defined by the ERCs.
  */
-export type Permission = {
-    type: string; // enum defined by ERCs
-    isAdjustmentAllowed: boolean; // whether the permission can be adjusted
-    data: Record<string, any>; // specific to the type, structure defined by ERCs
+export type BasePermission = {
+    type: string;
+    isAdjustmentAllowed: boolean;
+    data: Record<string, any>;
 };
 
 /**
  * A base rule type that all rules must extend.
  * `isAdjustmentAllowed` defines a boolean value that allows DApp to define whether the "rule" can be attenuated–adjusted to meet the user's terms.
- * @param type - is an enum defined by the ERCs
- * @param isAdjustmentAllowed - is a boolean that indicates whether the rule can be adjusted.
- * @param data - is a record of the data that is associated with the rule, and the structure is defined by the ERCs.
+ * @property type - is an enum defined by the ERCs
+ * @property isAdjustmentAllowed - is a boolean that indicates whether the rule can be adjusted.
+ * @property data - is a record of the data that is associated with the rule, and the structure is defined by the ERCs.
  */
 export type Rule = {
-    type: string; // enum defined by ERCs
-    isAdjustmentAllowed: boolean; // whether the rule can be adjusted
-    data: Record<string, any>; // specific to the type, structure defined by ERCs
+    type: string;
+    isAdjustmentAllowed: boolean;
+    data: Record<string, any>;
+};
+
+////////////////////////////////////////////////////
+// MetaMask Permission Types
+////////////////////////////////////////////////////
+
+/**
+ * Base data for all MetaMask permissions.
+ * @property justification - is a human-readable explanation of why the permission is being requested.
+ */
+export type MetaMaskBasePermissionData = {
+    justification?: string;
 };
 
 /**
- * Parameters for the `wallet_requestExecutionPermissions` JSON-RPC method.
- * @param chainId - chainId defines the chain with EIP-155 which applies to this permission request and all addresses can be found defined by other parameters.
- * @param address - address identifies the account being targetted for this permission request which is useful when a connection has been established and multiple accounts have been exposed. It is optional to let the user choose which account to grant permission for.
- * @param signer - signer is a field that identifies the key or account associated with the permission or alternatively the wallet will manage the session. See the "Signers" section for details.
- * @param permission - permission defines the allowed behavior the signer can do on behalf of the account. See the "Permission" section for details.
- * @param rules - rules defined the restrictions or conditions that a signer MUST abide by when using a permission to act on behalf of an account. See the "Rule" section for details.
+ * A permission to stream native tokens.
+ * @property data.initialAmount - is the initial amount of the native token to be streamed. Defaults to 0.
+ * @property data.maxAmount - is the maximum amount of the native token to be streamed. Defaults to Max Uint256.
+ * @property data.amountPerSecond - is the amount of the native token to be streamed per second.
+ * @property data.startTime - is the start time of the stream. Defaults to current time.
  */
-export type PermissionRequest = {
+export type NativeTokenStreamPermission = BasePermission & {
+    type: 'native-token-stream';
+    data: MetaMaskBasePermissionData & {
+      initialAmount?: Hex;
+      maxAmount?: Hex;
+      amountPerSecond: Hex;
+      startTime?: number;
+    };
+};
+
+/**
+ * A permission to stream native tokens periodically.
+ * @property data.periodAmount - is the amount of the native token to be streamed per period.
+ * @property data.periodDuration - is the duration of the period in seconds.
+ * @property data.startTime - is the start time of the stream. Defaults to current time.
+ */
+export type NativeTokenPeriodicPermission = BasePermission & {
+    type: 'native-token-periodic';
+    data: MetaMaskBasePermissionData & {
+      periodAmount: Hex;
+      periodDuration: number;
+      startTime?: number;
+    };
+};
+
+/**
+ * A permission to stream ERC20 tokens.
+ * @property data.initialAmount - is the initial amount of the ERC20 token to be streamed. Defaults to 0.
+ * @property data.maxAmount - is the maximum amount of the ERC20 token to be streamed. Defaults to Max Uint256.
+ * @property data.amountPerSecond - is the amount of the ERC20 token to be streamed per second.
+ * @property data.startTime - is the start time of the stream. Defaults to current time.
+ * @property data.tokenAddress - is the address of the ERC20 token to be streamed.
+ */
+export type Erc20TokenStreamPermission = BasePermission & {
+    type: 'erc20-token-stream';
+    data: MetaMaskBasePermissionData & {
+      initialAmount?: Hex;
+      maxAmount?: Hex;
+      amountPerSecond: Hex;
+      startTime?: number;
+      tokenAddress: Hex;
+    };
+};
+  
+/**
+ * A permission to stream ERC20 tokens periodically.
+ * @property data.periodAmount - is the amount of the ERC20 token to be streamed per period.
+ * @property data.periodDuration - is the duration of the period in seconds.
+ * @property data.startTime - is the start time of the stream. Defaults to current time.
+ * @property data.tokenAddress - is the address of the ERC20 token to be streamed per period.
+ */
+export type Erc20TokenPeriodicPermission = BasePermission & {
+    type: 'erc20-token-periodic';
+    data: MetaMaskBasePermissionData & {
+      periodAmount: Hex;
+      periodDuration: number;
+      startTime?: number;
+      tokenAddress: Hex;
+    };
+};
+
+/**
+ * A custom permission.
+ * @property data - is a record of the data that is associated with the permission, and the structure is defined by the ERCs.
+ */
+export type CustomPermission = BasePermission & {
+    type: 'custom';
+    data: MetaMaskBasePermissionData & Record<string, unknown>;
+};
+
+/**
+ * Represents the type of the ERC-7715 permissions that can be granted.
+ */
+export type PermissionTypes =
+    | NativeTokenStreamPermission
+    | NativeTokenPeriodicPermission
+    | Erc20TokenStreamPermission
+    | Erc20TokenPeriodicPermission
+    | CustomPermission;
+
+////////////////////////////////////////////////////
+// Permission Requests
+////////////////////////////////////////////////////
+
+/**
+ * Parameters for the `wallet_requestExecutionPermissions` JSON-RPC method.
+ * @property chainId - chainId defines the chain with EIP-155 which applies to this permission request and all addresses can be found defined by other parameters.
+ * @property address - address identifies the account being targetted for this permission request which is useful when a connection has been established and multiple accounts have been exposed. It is optional to let the user choose which account to grant permission for.
+ * @property signer - signer is a field that identifies the key or account associated with the permission or alternatively the wallet will manage the session. See the "Signers" section for details.
+ * @property permission - permission defines the allowed behavior the signer can do on behalf of the account. See the "Permission" section for details.
+ * @property rules - rules defined the restrictions or conditions that a signer MUST abide by when using a permission to act on behalf of an account. See the "Rule" section for details.
+ */
+export type PermissionRequest<
+    TSigner extends Signer,
+    TPermission extends PermissionTypes
+> = {
     chainId: Hex; // hex-encoding of uint256
     address?: Hex;
-    signer: Signer;
-    permission: Permission;
+    signer: TSigner;
+    permission: TPermission;
     rules?: Rule[];
 };
 
 /**
  * Response from the `wallet_requestExecutionPermissions` JSON-RPC method.
  * First note that the response contains all of the parameters of the original request and it is not guaranteed that the values received are equivalent to those requested.
- * @param context - is a catch-all to identify a permission for revoking permissions or submitting userOps, and can contain non-identifying data as well. It MAY be the `context` as defined in ERC-7679 and ERC-7710.
- * @param dependencyInfo - is an array of objects, each containing fields for `factory` and `factoryData` as defined in ERC-4337. Either both `factory` and `factoryData` must be specified in an entry, or neither. This array is used describe accounts that are not yet deployed but MUST be deployed in order for a permission to be successfully redeemed.
- * @param signerMeta - is dependent on the account type. If the signer type is `wallet` then it's not required. If the signer type is `key` or `keys` then `userOpBuilder` is required as defined in ERC-7679. If the signer type is `account` then `delegationManager` is required as defined in ERC-7710.
+ * @property context - is a catch-all to identify a permission for revoking permissions or submitting userOps, and can contain non-identifying data as well. It MAY be the `context` as defined in ERC-7679 and ERC-7710.
+ * @property dependencyInfo - is an array of objects, each containing fields for `factory` and `factoryData` as defined in ERC-4337. Either both `factory` and `factoryData` must be specified in an entry, or neither. This array is used describe accounts that are not yet deployed but MUST be deployed in order for a permission to be successfully redeemed.
+ * @property signerMeta - is dependent on the account type. If the signer type is `wallet` then it's not required. If the signer type is `key` or `keys` then `userOpBuilder` is required as defined in ERC-7679. If the signer type is `account` then `delegationManager` is required as defined in ERC-7710.
  */
-export type PermissionResponse = PermissionRequest & {
+export type PermissionResponse<
+    TSigner extends Signer,
+    TPermission extends PermissionTypes
+> = PermissionRequest<TSigner, TPermission> & {
     context: Hex;
     dependencyInfo: {
         factory: Hex;
@@ -117,7 +241,7 @@ export type PermissionResponse = PermissionRequest & {
 
 /**
  * Parameters for the `wallet_revokeExecutionPermission` JSON-RPC method.
- * @param permissionContext - the context identifier for the permission to be revoked
+ * @property permissionContext - the context identifier for the permission to be revoked
  */
 export type RevokeExecutionPermissionRequestParams = {
     permissionContext: Hex;
