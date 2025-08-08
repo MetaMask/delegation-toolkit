dbc007524c4b622fd2f37e9654d77fb58041b842
Jeff Smale <6363749+jeffsmale90@users.noreply.github.com>
2025-08-08 15:45:08 +1200

    Add test ERC721Token contract

---
+++ packages/delegator-e2e/contracts/src/ERC721Token.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC721Token is ERC721, Ownable {
+    constructor(uint256 initialSupply) ERC721("MyToken", "MTK") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
